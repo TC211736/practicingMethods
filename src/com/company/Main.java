66040d0f38e7a94feee75ddfb3090c595928a728
TC211736 <TC211736@reigate.ac.uk>
2021-11-12 13:28:16 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.List;
+public class Main {
+    static List CounterColours = new ArrayList() {
+
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
